49f117ce4f871e103f6ccfe0a7becc741b8b346b
Khadeem Lattibeaudiere <khadeemlatti20@gmail.com>
2023-11-19 23:17:37 -0500

    completed site, might adjust later

--- world.js
+++ world.js
@@ -1,14 +1,15 @@
 
 window.onload= function(){
     
-    var button = document.getElementById("lookup");
-
+    var countryButton = document.getElementById("lookup");
+    var cityButton = document.getElementById("cityLookup")
     var country= document.getElementById('country').value.trim();
+    const Url= "http://localhost/info2180-lab5/world.php?country=";
 
-    button.addEventListener("click", function(event){
+    countryButton.addEventListener("click", function(event){
         event.preventDefault();
         var country= document.getElementById('country').value.trim();
-        const Url= "http://localhost/info2180-lab5/world.php?country=";
+        
         const Request= new XMLHttpRequest();
 
         Request.onreadystatechange= function(){
@@ -16,18 +17,35 @@ window.onload= function(){
             if (Request.readyState === XMLHttpRequest.DONE) {
                 if (Request.status === 200) {
                     let response = Request.responseText;
-                    console.log(response);
-                    document.getElementById("result").innerText = response;
+                    //debug
+                    //console.log(response);
+                    document.getElementById("result").innerHTML = response;
                 } 
                 else{
                     let response = "YOUR CODE IS Rubbish Bruv!!";
-                    document.getElementById("result").innerText = response;
+                    document.getElementById("result").innerHTML = response;
                 }
             }
         }
         Request.open("GET",Url + country);
         Request.send();
     })
+
+    cityButton.addEventListener("click", function(event){
+        event.preventDefault();
+
+        if(country !== ""){
+            const xhttp2 = new XMLHttpRequest();
+            xhttp2.onreadystatechange = function(){
+                if(this.readyState == 4 && this.status == 200){
+                    document.getElementById("result").innerHTML = this.responseText;
+                }
+            }
+            xhttp2.open("GET", Url + country + "&lookup=cities");
+            xhttp2.send();
+        }
+    })
+
 
 
 }
--- world.php
+++ world.php
@@ -8,32 +8,58 @@ $dbname = 'world';
 
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-
+$city= false;
 
 if ( isset($_GET['country'])) {
   $searched= $_GET['country'];
-  //$val = $_GET['country'])
+
   $country= filter_var($searched, FILTER_SANITIZE_STRING);
-  
   $sql = "SELECT * FROM countries WHERE name LIKE '%$country%'";
-  $stmt= $conn->prepare($sql);
+  
+  if(isset($_GET['lookup'])){
+    $lookup = $_GET['lookup'];
+    $city = true;
+    $headings = array("Name", "District", "Population");
+
+    $stmt = $conn->query($sql);
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $strResult = $results[0]['code'];
+
+    $sql = "SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON countries.code = cities.country_code WHERE countries.code LIKE '%$strResult%'";
+
+  }
+  else{
+    $headings = array("Name", "Continent", "Independence Year", "Head of State");
+  }
+
+  $stmt = $conn->query($sql);
 }
-
-else{
-  $stmt = $conn->query("SELECT * FROM countries");
-
-}
-
-
-
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-// Debugging statements
-echo "Number of rows returned: " . count($results) . PHP_EOL;
-echo "Results structure: " . PHP_EOL;
-var_dump($results);
+//var_dump($results);
 
 ?>
 
-
+<table>
+  <tr>
+    <?php foreach($headings as $heading): ?>
+      <th><?=$heading; ?></th>
+    <?php endforeach; ?>	
+  </tr>
+<?php foreach ($results as $row): ?>
+  <tr>
+  <?php
+    if(!$city): ?>
+      <td><?= $row['name']; ?></td>
+      <td><?= $row['continent']; ?></td>
+      <td><?= $row['independence_year']; ?></td>
+      <td><?= $row['head_of_state']; ?></td>
+    <?php else: ?>
+      <td><?= $row['name']; ?></td>
+      <td><?= $row['district']; ?></td>
+      <td><?= $row['population']; ?></td>
+    <?php endif; ?>
+  </tr>
+  <?php endforeach; ?>
+</table>
